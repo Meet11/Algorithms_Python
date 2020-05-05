430813d1f3a888aed6729ad749bf0f83f87d692e
Meet Gujrathi <meetgujrathi@gmail.com>
2020-05-05 12:31:14 +0530

    Update DP_Max_Subset_Sum_No_Adjacent.py

--- DP_Max_Subset_Sum_No_Adjacent.py
+++ DP_Max_Subset_Sum_No_Adjacent.py
@@ -6,18 +6,17 @@
 #Consider - [2, 1, 5, 6, 4]
 #maxSums = [2, 2, 7, 8, 11]
 #initialise the first element to the arrays first element, and the second to be the max of first element and the second element to get the base scenario for our solution
-
 def maxSubsetSumNoAdjacent(array):
-  if not len(array):
-	return 0
-  if len(array) == 1:
-	return array[0]
-  if len(array) == 2:
-	return array[1]
-  first = array[0]
-  second = max(array[0], array[1])
-  for i in range(2, len(array)):
-  	maxSums = max(first + array[i], second)
-	first = second
-	second = maxSums
-  return maxSums
+    if not len(array):
+		return 0
+	if len(array) == 1:
+		return array[0]
+	if len(array) == 2:
+		return array[1]
+	first = array[0]
+	second = max(array[0], array[1])
+	for i in range(2, len(array)):
+		maxSums = max(first + array[i], second)
+		first = second
+		second = maxSums
+	return maxSums
